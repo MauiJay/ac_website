b3eb2cb36615db6baefb994a1a4c89f090da1fc6
Jay Lamping <jaycadla@gmail.com>
2021-02-21 14:57:29 -1000

    complete some stuff

--- public/index.php
+++ public/index.php
@@ -25,7 +25,7 @@ chdir(__DIR__);
 
 // Load our paths config file
 // This is the line that might need to be changed, depending on your folder structure.
-require realpath(FCPATH . '../app/Config/Paths.php') ?: FCPATH . '../app/Config/Paths.php';
+require realpath(FCPATH . '../dist/app/Config/Paths.php') ?: FCPATH . '../dist/app/Config/Paths.php';
 // ^^^ Change this if you move your application folder
 
 $paths = new Config\Paths();
